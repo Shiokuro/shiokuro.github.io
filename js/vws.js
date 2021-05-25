7af471a7cadca35006d70fa1f1a12bea8a43d617
Shiokuro <57196760+Shiokuro@users.noreply.github.com>
2021-05-25 17:29:24 -0300

    mobile nav(needs adjustment probably) + proper arrows

--- js/vws.js
+++ js/vws.js
@@ -1,12 +1,27 @@
-$(document).ready(function() {
-  
-  $('#pagepiling').pagepiling({
-    scrollingSpeed: 400,
-    easing: 'linear',
-    navigation: false,
+
+if (screen && screen.width > 768) {
+  $(document).ready(function() {
+    $('#pagepiling').pagepiling({
+      scrollingSpeed: 400,
+      easing: 'linear',
+      navigation: false,
+    });
   });
-});
+  } else{ 
+    $(document).ready(function() {
+      $('#pagepiling').pagepiling({
+        scrollingSpeed: 400,
+        easing: 'linear',
+        navigation: false,
+        direction: 'horizontal',
+      });
+    });
+  }
 
 $(".arrows").click(function() {
   $.fn.pagepiling.moveSectionDown();
+})
+
+$(".menuIcon").click(function(){
+  $(".mobileButtons").toggleClass("noDisplay")
 })
