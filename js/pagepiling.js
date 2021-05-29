f09595caaa5f1a1bff247959daa57d9f4faa00d1
Shiokuro <57196760+Shiokuro@users.noreply.github.com>
2021-05-29 20:07:27 -0300

    ajax

--- js/pagepiling.js
+++ js/pagepiling.js
@@ -41,3 +41,16 @@ if (screen && screen.width > 768) {
   $( ".artistas" ).on('click', function() {
     $( "#body" ).load( "artistas.html" );
   });
+
+  function loadDoc(pageName) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("body").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", pageName + ".html", true);
+    xhttp.send();
+
+    $.getScript('js/pagepiling.js')
+  }
